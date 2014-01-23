572c3f2cd089b8c7fa629a09418100679fa3ad43
al223ec <al223ec@student.lnu.se>
2014-01-23 15:14:06 +0100

    Kassa

--- Kassakvitto/Kassakvitto/Default.aspx.designer.cs
+++ Kassakvitto/Kassakvitto/Default.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace Kassakvitto {
         protected global::System.Web.UI.WebControls.TextBox InputTextBox;
         
         /// <summary>
+        /// InputValidator control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator InputValidator;
+        
+        /// <summary>
         /// SubmitButton control.
         /// </summary>
         /// <remarks>
